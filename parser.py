35adb57d5fa80d88a338ed74c2fdbb1eacd24db6
Ben1152000 <benjamindarnell00@gmail.com>
2021-08-05 17:23:37 -0700

    Add gitignore, debug mode

--- parser.py
+++ parser.py
@@ -2,11 +2,14 @@ from pyDigitalWaveTools.vcd.parser import VcdParser
 from wiretrace import WireTrace
 import json
 
+DEBUG = False
+
 def parse_vcd(filename):
     with open(filename) as vcd_file:
         vcd = VcdParser()
         vcd.parse(vcd_file)
         data = vcd.scope.toJson()  # dump json here for debugging
-        with open('dump.json', 'w') as writefile:
-            writefile.write(json.dumps(data, indent=2, sort_keys=True))
+        if DEBUG:
+            with open('dump.json', 'w') as writefile:
+                writefile.write(json.dumps(data, indent=2, sort_keys=True))
         return WireTrace.parse_vcd(data)
