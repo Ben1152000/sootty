cb9eb7a4d537dd8cc4438cf8ce09a1f745dad427
Ben1152000 <benjamindarnell00@gmail.com>
2021-08-05 18:05:25 -0700

    IT BEGINS

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# Dependencies:
+
+- viu (use provided git repo)
+- pyDigitalWaveTools (must be installed from git repo, cd into the directory and use the command `python3 -m pip install -e .` to install)
+- rsvg-convert (`brew install librsvg`)
--- run.py
+++ run.py
@@ -2,6 +2,8 @@ import sys
 from wiretrace import WireTrace
 from parser import parse_vcd
 from visualizer import Visualizer
+from subprocess import Popen, STDOUT
+from tempfile import TemporaryFile
 
 if __name__ == "__main__":
     
@@ -9,6 +11,19 @@ if __name__ == "__main__":
         print('Give me a vcd file to parse')
         sys.exit(-1)
     
-    print(Visualizer().wiretrace_to_svg(
-            wiretrace=parse_vcd(sys.argv[1]),
-            length=20))
+    svg_data = Visualizer().wiretrace_to_svg(
+        wiretrace=parse_vcd(sys.argv[1]), length=20)
+    
+    print(svg_data)
+    
+    # with open('temp.svg', 'w') as svg_file:
+    #     svg_file.write(svg_data)
+    
+    # with open('temp.svg', 'r') as svg_file:
+    #     process = Popen(
+    #         'rsvg-convert | kitty +kitten icat', 
+    #         stdin=svg_file, 
+    #         stdout=sys.stdout, 
+    #         stderr=sys.stderr, 
+    #         shell=True
+    #     )
