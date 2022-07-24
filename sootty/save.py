e6ee9f159813fa1c3e5874418f6cfca3d131280c
karthik <karthiklaksh1729@gmail.com>
2022-07-24 13:46:42 +0530

    Added delete after 500 saved query functionality

--- sootty/save.py
+++ sootty/save.py
@@ -1,11 +1,19 @@
 import os
+import subprocess
 import datetime
 
 def save_query(save, name, wires, br, length, start, end, display):
     savefile = os.getenv("HOME") + "/.config/sootty/save/queries.yaml"
     """
-    Constructing the query using conditionals
+    Memory check for the file
     """
+    with open(savefile) as f:
+        count = sum(1 for _ in f)
+        if count >= 1500:
+            status = subprocess.run("sed -i 1,3d " + savefile, shell=True)  # Deleting first 3 lines using sed instead of reading all lines
+            if status.returncode != 0:                                      # sed operation failed
+                raise Exception("Error deleting least recent query.")
+                
     if is_save_file(savefile):
         with open(savefile, "a") as stream:
             query_write(stream, save, name, wires, br, length, start, end, display)
@@ -16,6 +24,9 @@ def save_query(save, name, wires, br, length, start, end, display):
 def query_write(savefile, save, name, wires, br, length, start, end, display):
     savefile.write(save + ":\n")
     savefile.write("  query:")
+    """
+    Constructing the query using conditionals
+    """  
     if name:
         savefile.write(' -f "' + name + '"')
     if wires:
