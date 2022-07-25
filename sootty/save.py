29d11a81245a87e6372e918dc3692edcacd7dff4
karthik <karthiklaksh1729@gmail.com>
2022-07-26 01:14:20 +0530

    Trying to add overwrite features, buggy rn

--- sootty/save.py
+++ sootty/save.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import datetime
+import yaml
 
 def save_query(save, name, wires, br, length, start, end, display):
     savefile = os.getenv("HOME") + "/.config/sootty/save/queries.yaml"
@@ -15,35 +16,49 @@ def save_query(save, name, wires, br, length, start, end, display):
                 raise Exception("Error deleting least recent query.")
                 
     if is_save_file(savefile):
-        with open(savefile, "a") as stream:
-            query_write(stream, save, name, wires, br, length, start, end, display)
+        with open(savefile, "a+") as stream:
+            query_write(savefile, stream, save, name, wires, br, length, start, end, display)
     else:
         with open(savefile, "w") as stream:
-            query_write(stream, save, name, wires, br, length, start, end, display)
+            query_write(savefile, stream, save, name, wires, br, length, start, end, display)
 
-def query_write(savefile, save, name, wires, br, length, start, end, display):
+def query_write(savefile_path, savefile, save, name, wires, br, length, start, end, display):
+    with open(savefile_path, "r+") as stream:
+        lines = yaml.safe_load(stream) 
+        if save in lines:
+            lines[save]["query"] = query_build(name, wires, br, length, start, end, display)
+            lines[save]["date"] = str(datetime.datetime.now())
+            stream.truncate(0)
+            res = sorted(lines.items(), key = lambda x: x[1]["date"])
+            print(res)
+            #yaml.dump(lines, savefile)            # Dumping the overwritten query to the file, forcing no inline output
+            exit(0)
     savefile.write(save + ":\n")
-    savefile.write("  query:")
-    """
-    Constructing the query using conditionals
-    """  
-    if name:
-        savefile.write(' -f "' + name + '"')
-    if wires:
-        savefile.write(' -w "' + wires + '"')
-    if br:
-        savefile.write(' -b "' + br + '"')
-    if length:
-        savefile.write(' -l "' + str(length) + '"')
-    if start:
-        savefile.write(' -s "' + start + '"')
-    if end:
-        savefile.write(' -e "' + end + '"')
-    if display:
-        savefile.write(" -d")
+    savefile.write("  query:")    
+    savefile.write(query_build(name, wires, br, length, start, end, display))
     savefile.write("\n")
     savefile.write("  date: " + str(datetime.datetime.now()) + "\n")
 
+def query_build(name, wires, br, length, start, end, display):
+    """
+    Constructing the query using conditionals
+    """
+    cmd = ""
+    if name:
+        cmd += ' -f "' + name + '"'
+    if wires:
+        cmd += ' -w "' + wires + '"'
+    if br:
+        cmd += ' -b "' + br + '"'
+    if length:
+        cmd += ' -l "' + str(length) + '"'
+    if start:
+        cmd += ' -s "' + start + '"'
+    if end:
+        cmd += ' -e "' + end + '"'
+    if display:
+        cmd += " -d"
+    return cmd
 
 def is_save_file(filename):
     return os.path.isfile(filename)
