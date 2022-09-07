f9804cf6a4e76f94c32eff609182416b82ac1f38
Yihua Liu <yihuajack@live.cn>
2022-09-07 22:38:49 +0800

    fix: test_style breakpoints

--- tests/test_style.py
+++ tests/test_style.py
@@ -4,13 +4,13 @@ from sootty import WireTrace, Visualizer, Style
 def test_style_class():
     wiretrace = WireTrace.from_vcd("example/example1.vcd")
 
-    Visualizer(Style.Silicon).to_svg(wiretrace, start=0, length=8, breakpoints=[10, 11]).display()
+    Visualizer(Style.Silicon).to_svg(wiretrace, start=0, length=8, breakpoints=[4, 5]).display()
 
-    Visualizer(Style.Dark).to_svg(wiretrace, start=0, length=8, breakpoints=[10, 11]).display()
+    Visualizer(Style.Dark).to_svg(wiretrace, start=0, length=8, breakpoints=[4, 5]).display()
 
-    Visualizer(Style.Light).to_svg(wiretrace, start=0, length=8, breakpoints=[10, 11]).display()
+    Visualizer(Style.Light).to_svg(wiretrace, start=0, length=8, breakpoints=[4, 5]).display()
 
-    Visualizer(Style.Colorful).to_svg(wiretrace, start=0, length=8, breakpoints=[10, 11]).display()
+    Visualizer(Style.Colorful).to_svg(wiretrace, start=0, length=8, breakpoints=[4, 5]).display()
 
 
 if __name__ == "__main__":
