83a3fcef16f4e79845dfc9896c8d26df4f80f108
Ben1152000 <benjamindarnell00@gmail.com>
2022-11-11 15:47:21 -0600

    Use unittest module for tests

---
+++ test/__init__.py
@@ -0,0 +1,6 @@
+from .test_all import TestStringMethods
+from .test_general import TestGeneral
+from .test_limits import TestLimits
+from .test_pyrtl import TestPyrtl
+from .test_style import TestStyle
+from .test_wires import TestWires
